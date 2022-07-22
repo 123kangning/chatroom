1f04eccb98f3c2ab10f734a2222f327e3f59ed86
123kangning <3390142252@qq.com>
2022-07-22 19:23:11 +0800

    1

--- chatroom/src/test/java/mysql/Test1.java
+++ chatroom/src/test/java/mysql/Test1.java
@@ -1,9 +1,15 @@
 package mysql;
 
+import com.fasterxml.jackson.databind.ser.std.ObjectArraySerializer;
 import lombok.extern.slf4j.Slf4j;
 import message.ResponseMessage;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
+import java.util.Scanner;
 
 import static server.ChatServer.connection;
 
@@ -15,6 +21,18 @@ public class Test1 {
     public static void main(String[] args)throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection= DriverManager.getConnection(URL,NAME,PASSWORD);
+        String sql="insert into message(msg_type,is_send,create_date,content,talker) values(?,?,?,?,?)";
+        PreparedStatement ps=connection.prepareStatement(sql);
+        ps.setString(1,"S");
+        ps.setString(2,"S");
+        ps.setDate(3,new Date(System.currentTimeMillis()));
+
+        //ps.setString(4,"只是测试");
+        byte[] bytes= FileCopyUtils.copyToByteArray(new File("src/description.txt"));
+        ps.setBytes(4,bytes);
+        ps.setString(5,"F");
+        int row=ps.executeUpdate();
+        log.info("row = "+row);
 
     }
 }
---
+++ chatroom/src/test/java/mysql/Test2.java
@@ -0,0 +1,42 @@
+package mysql;
+
+import com.fasterxml.jackson.databind.ser.std.ObjectArraySerializer;
+import lombok.extern.slf4j.Slf4j;
+import message.ResponseMessage;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.sql.*;
+import java.util.Scanner;
+
+import static server.ChatServer.connection;
+
+@Slf4j
+public class Test2 {
+    private static final String URL="jdbc:mysql://localhost:3306/test";
+    private static final String NAME="root";
+    private static final String PASSWORD="9264wkn.";
+    public static void main(String[] args)throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection= DriverManager.getConnection(URL,NAME,PASSWORD);
+        String sql="select content from message where msg_id=6";
+        PreparedStatement ps=connection.prepareStatement(sql);
+        ResultSet set=ps.executeQuery();
+        /*if(set.next()){
+            String s=set.getString(1);
+            log.info("message={}",s);
+        }*/
+        if(set.next()){
+            log.info("有的");
+            //ObjectInputStream ois=new ObjectInputStream(set.getObject(1));
+            byte[] bytes= set.getBytes(1);
+           /* Scanner scanner=new Scanner(new BufferedInputStream(bytes));
+            while(scanner.hasNext())
+                System.out.println(scanner.nextLine());*/
+            log.info(new String(bytes));
+        }else{
+            log.info("没有的");
+        }
+    }
+}
